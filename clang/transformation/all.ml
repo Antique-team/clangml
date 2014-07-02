5887a519f8c1ba7d46c531677a748e96f17571b0
Francois Berenger <berenger@di.ens.fr>
2014-07-02 17:33:02 +0200

    preIncrement became preIncrDecr and support both ++a and --a

--- clang/transformation/all.ml
+++ clang/transformation/all.ml
@@ -8,5 +8,5 @@ let transform_decl clang decl =
   |> SimplifyDeclStmt.transform_decl clang
   |> SplitInitialisers.transform_decl clang
   |> LiftConditionals.transform_decl clang
-  |> PreIncrement.transform_decl clang
+  |> PreIncrDecr.transform_decl clang
   (*|> NameAnonymousTypes.transform_decl clang*)
