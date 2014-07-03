ab3205f2a63850a06b79c4a482eb034b8580e875
Francois Berenger <berenger@di.ens.fr>
2014-07-03 12:26:06 +0200

    splitInitializers is after postIncrDecr

--- clang/transformation/all.ml
+++ clang/transformation/all.ml
@@ -6,8 +6,8 @@ let transform_decl clang decl =
   |> SwitchToIf.transform_decl clang
   |> RemoveImplicitCast.transform_decl clang
   |> SimplifyDeclStmt.transform_decl clang
-  |> SplitInitialisers.transform_decl clang
   |> LiftConditionals.transform_decl clang
   |> PostIncrDecr.transform_decl clang
+  |> SplitInitialisers.transform_decl clang
   |> PreIncrDecr.transform_decl clang
   (*|> NameAnonymousTypes.transform_decl clang*)
