327fb809bfea9b159f74e3e41835931714102694
Francois Berenger <berenger@di.ens.fr>
2014-07-07 13:38:29 +0200

    commented out switchToIf since it is not finished

--- clang/transformation/all.ml
+++ clang/transformation/all.ml
@@ -3,7 +3,7 @@ open Util.Prelude
 let transform_decl clang decl =
   decl
   |> ForToWhile.transform_decl clang
-  |> SwitchToIf.transform_decl clang
+  (* |> SwitchToIf.transform_decl clang *)
   |> RemoveImplicitCast.transform_decl clang
   |> SimplifyDeclStmt.transform_decl clang
   |> LiftConditionals.transform_decl clang
