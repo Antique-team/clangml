bab51a37d5e263b592356b8f9d62fd55e4d9124c
Francois Berenger <francois.berenger@inria.fr>
2014-07-08 18:55:15 +0200

    added batteries as a dep. plus started to implement ctyp_of_sum_type_branches

--- tools/ocamlTypes/codegen.ml
+++ tools/ocamlTypes/codegen.ml
@@ -1,17 +1,32 @@
 open Camlp4.PreCast
 open Sig
 
+module L = BatList
+
 
 let _loc = Loc.ghost
 
 
+let ctyp_of_sum_type_branches branches =
+  List.map
+    (fun b -> <:ctyp<$uid:b.stb_name$>>)
+    branches
+  |>
+  L.reduce
+    (fun acc ty -> <:ctyp< $acc$ | $ty$ >>)
+
 let str_item_of_ocaml_type = function
-  | AliasType (loc, name, ty) ->
-      <:str_item<type aliastype = something>>
-  | SumType sum_type ->
-      <:str_item<type sumtype = A | B>>
-  | RecordType record_type ->
-      <:str_item<type recordtype = { a : int }>>
+  | AliasType (loc, name, ty) -> assert false;
+  | SumType st ->
+   (*
+    type language =
+    | Lang_C
+    | Lang_CXX
+   *)
+      let branches = ctyp_of_sum_type_branches st.st_branches in
+      <:str_item<type $lid:st.st_name$ = | $branches$ >>
+  | RecordType rt ->
+      <:str_item<type $lid:rt.rt_name$ = { a : int }>>
   | RecursiveType (loc, types) ->
       <:str_item<type rec1 = C | D of rec2 and rec2 = E | F of rec1>>
   | Version (loc, version) ->
