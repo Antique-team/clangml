7714fb49795f13d40ea82681ee20bbe37a2f2520
Pippijn van Steenhoven <pip88nl@gmail.com>
2014-07-08 16:08:38 +0200

    print ocaml_types

--- tools/simplify/generateSimplified.ml
+++ tools/simplify/generateSimplified.ml
@@ -6,19 +6,9 @@ open OcamlTypes.Process
 let codegen ocaml_types =
   let _loc = Loc.ghost in
 
-  let type_map = make_type_map ocaml_types in
+  let visit_types = OcamlTypes.Type_graph.must_visit ocaml_types in
 
-  let simplified : sum_type list =
-    List.map (fun (name, _, { st_loc = loc; st_branches = branches }) ->
-        {
-          st_loc = loc;
-          st_name = name;
-          st_branches = branches;
-        }
-    ) type_map
-  in
-
-  (*print_endline @@ Show_type_map.show_list type_map;*)
+  print_endline @@ Show_ocaml_types.show ocaml_types;
 
   <:str_item<
     open Ast
