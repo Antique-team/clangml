024822481347f13c23e795cc7b45941cf599dc0e
Francois Berenger <francois.berenger@inria.fr>
2014-07-08 17:01:21 +0200

    implemented codegen and added batteries dep. in the _tags file

--- tools/simplify/generateSimplified.ml
+++ tools/simplify/generateSimplified.ml
@@ -2,11 +2,33 @@ open Camlp4.PreCast
 open OcamlTypes.Sig
 open OcamlTypes.Process
 
+module S = BatString
 
 let codegen ocaml_types =
   let _loc = Loc.ghost in
 
   let visit_types = OcamlTypes.Type_graph.must_visit ocaml_types in
+
+  let ocaml_types =
+    List.filter
+      (fun (name, ty) -> not (List.mem_assoc (name ^ "_") ocaml_types))
+      ocaml_types
+  in
+
+  let ocaml_types =
+    List.map
+      (fun ((name, ty) as name_type) -> 
+         if not (S.ends_with name "_") then
+           name_type
+         else
+           let name = S.rchop name in
+           match ty with
+           | SumType st -> (name, SumType { st with st_name = name })
+           | RecordType rt -> (name, RecordType { rt with rt_name = name })
+           | _ -> assert(false);
+      )
+      ocaml_types
+  in
 
   print_endline @@ Show_ocaml_types.show ocaml_types;
 
