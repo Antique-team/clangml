af3642617ad9be68e2fcaf629566da1be6df6774
Francois BERENGER <francois.berenger@inria.fr>
2014-09-02 14:48:33 +0200

    simpler test

--- plugin/testsuite/ObjCisa.m
+++ plugin/testsuite/ObjCisa.m
@@ -1,42 +1,16 @@
-
-// @interface NSNumber
-// + (NSNumber *)numberWithInt:(int)value;
-// @end
-
-// @protocol NSCopying @end
-// typedef unsigned long NSUInteger;
-
-// @interface NSDictionary
-// + (id)dictionaryWithObjects:(const id [])objects
-//                     forKeys:(const id <NSCopying> [])keys
-//                       count:(NSUInteger)cnt;
-// - (Class)isa;
-// @end
-
-// @interface NSString<NSCopying>
-// @end
-
-// void f() {
-//   NSDictionary *dict1 = @{ @"toto":@111 };
-//   Class isa = [dict1 isa];
-// }
-
 typedef struct objc_class *Class;
 
 typedef struct objc_object {
-    Class isa;
+  Class isa;
 } *id;
 
-// rdar 7609722
-@interface Foo { 
-@public 
-  id isa; 
-} 
+@interface Foo {
+@public
+  id isa;
+}
 +(id)method;
 @end
 
 id Test2() {
-    if([Foo method]->isa)
-      return (*[Foo method]).isa;
-    return [Foo method]->isa;
+  return [Foo method]->isa;
 }
