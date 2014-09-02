c073d6517b11d7d5a5fce9252038cd572ea3e4fd
Francois BERENGER <francois.berenger@inria.fr>
2014-09-02 14:41:39 +0200

    this one really triggers TODO: TraverseObjCIsaExpr

--- plugin/testsuite/ObjCisa.m
+++ plugin/testsuite/ObjCisa.m
@@ -1,22 +1,42 @@
 
-@interface NSNumber
-+ (NSNumber *)numberWithInt:(int)value;
+// @interface NSNumber
+// + (NSNumber *)numberWithInt:(int)value;
+// @end
+
+// @protocol NSCopying @end
+// typedef unsigned long NSUInteger;
+
+// @interface NSDictionary
+// + (id)dictionaryWithObjects:(const id [])objects
+//                     forKeys:(const id <NSCopying> [])keys
+//                       count:(NSUInteger)cnt;
+// - (Class)isa;
+// @end
+
+// @interface NSString<NSCopying>
+// @end
+
+// void f() {
+//   NSDictionary *dict1 = @{ @"toto":@111 };
+//   Class isa = [dict1 isa];
+// }
+
+typedef struct objc_class *Class;
+
+typedef struct objc_object {
+    Class isa;
+} *id;
+
+// rdar 7609722
+@interface Foo { 
+@public 
+  id isa; 
+} 
++(id)method;
 @end
 
-@protocol NSCopying @end
-typedef unsigned long NSUInteger;
-
-@interface NSDictionary
-+ (id)dictionaryWithObjects:(const id [])objects
-                    forKeys:(const id <NSCopying> [])keys
-                      count:(NSUInteger)cnt;
-- (Class)isa;
-@end
-
-@interface NSString<NSCopying>
-@end
-
-void f() {
-  NSDictionary *dict1 = @{ @"toto":@111 };
-  Class isa = dict1.isa;
+id Test2() {
+    if([Foo method]->isa)
+      return (*[Foo method]).isa;
+    return [Foo method]->isa;
 }
