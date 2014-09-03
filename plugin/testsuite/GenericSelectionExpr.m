7e7b194c557d3b48238eed173f11a350ca25adb9
Francois BERENGER <francois.berenger@inria.fr>
2014-09-03 11:05:10 +0200

    more complete test case for GenericSelectionExpr

--- plugin/testsuite/GenericSelectionExpr.m
+++ plugin/testsuite/GenericSelectionExpr.m
@@ -1,4 +1,6 @@
 
 void f() {
-  int i =  _Generic(1.0, double: 1, float: 2, default: 3);
+  int i =  _Generic(1.0 , double: 1, float: 2, default: 3);
+  int j =  _Generic(1.0f, double: 1, float: 2, default: 3);
+  int k =  _Generic(1   , double: 1, float: 2, default: 3);
 }
