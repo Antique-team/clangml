dc167bf1a692055463e2df965783e7f15144db6e
Francois BERENGER <francois.berenger@inria.fr>
2015-08-24 18:54:57 +0200

    no more ocamlbuild pack for OcamlTypes, the module is built explicitely instead

---
+++ ocamlTypes.ml
@@ -0,0 +1,7 @@
+
+module Codegen = struct include Codegen_impl end
+module Parse = struct include Parse end
+module Print = struct include Print end
+module Process = struct include Process end
+module Sig = struct include Sig end
+module Type_graph = struct include Type_graph end
