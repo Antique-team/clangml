f53c253096ab7d0f57901588d5c02595fde4a054
François Berenger <berenger@mac-cm.ens.fr>
2016-10-24 15:00:39 +0200

    some fixes for compilation on osx

--- plugin/c++/PluginRegistration.cpp
+++ plugin/c++/PluginRegistration.cpp
@@ -13,7 +13,9 @@ clang_registerCheckers (clang::ento::CheckerRegistry &registry)
 {
 
 #ifndef __FreeBSD__
+#ifndef __APPLE__
   backtrace_init ();
+#endif // __APPLE__
 #endif  // __FreeBSD__
   registry.addChecker<OCamlChecker> ("external.OCaml", "Runs static analysis passes written in OCaml");
 }
